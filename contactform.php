bfd6e139337919770ebe6a298167fdccb010354f
t.savaryn@gmail.com <t.savaryn@gmail.com>
2022-08-21 00:55:56 -0400

    form

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("info@tetianasavaryn.com", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: tetiana@tetianasavaryn.com");
+
+
+}
+
+
+?>
